82cf610c554be2cdd357c85762edf88ce348d8a9
Sakshi Kakde <kakdesakshi@gmail.com>
2021-02-14 19:09:49 -0500

    variable name change

--- Phase2/Code/Misc/DataUtils.py
+++ Phase2/Code/Misc/DataUtils.py
@@ -35,10 +35,10 @@ def SetupAll(BasePath, CheckPointPath):
     Trainabels - Labels corresponding to Train
     """
     # Setup DirNames
-    DirNamesTrain =  SetupDirNames(BasePath + "/Train_dummy/ImageFileNames.csv")
+    DirNamesTrain =  SetupDirNames(BasePath + "/Train_synthetic/ImageFileNames.csv")
 
     # Read and Setup Labels
-    TrainLabels = ReadLabels(BasePath + "/Train_dummy/H4.csv")
+    TrainLabels = ReadLabels(BasePath + "/Train_synthetic/H4.csv")
 
     # If CheckPointPath doesn't exist make the path
     if(not (os.path.isdir(CheckPointPath))):
